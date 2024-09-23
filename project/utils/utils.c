a97e208c8d746dfb4f2f5c1085c05e4a12e4e8c4
CC4K <cedric.kheirallah@student.uclouvain.be>
2024-09-23 15:50:42 +0200

    matrix multiplication + stringification

--- project/utils/utils.c
+++ project/utils/utils.c
@@ -18,13 +18,13 @@ int foo(void) { return 42; }
  * @note `mat1`, `mat2` and `patterns` should point to the body of `request` at the location of each element.
 */
 void parse_request(struct parsed_request *parsed, char *request, size_t request_len) {
-    char buffer[11];
+    char buffer[50];
     int buf_index = 0;
     int comma_count = 0;
     int mat_size = 0;
     int cut_size = 0;
-    int matrix_index = 0;
-    int pattern_index = 0;
+//    int matrix_index = 0;
+//    int pattern_index = 0;
 
     for (size_t i = 0; i < request_len; i++) {
         if (request[i] == ',' || i == request_len-1) {
@@ -36,32 +36,38 @@ void parse_request(struct parsed_request *parsed, char *request, size_t request_
 
             // first ,
             if (comma_count == 0) {
-                parsed->matrices_size = (uint32_t)strtol(buffer, NULL, 10);
+                parsed->matrices_size = (uint32_t) strtol(buffer, NULL, 10);
                 mat_size = parsed->matrices_size;
                 cut_size = mat_size*mat_size*4;
             }
             // second ,
             else if (comma_count == 1) {
-                parsed->nb_patterns = (uint32_t)strtol(buffer, NULL, 10);
+                parsed->nb_patterns = (uint32_t) strtol(buffer, NULL, 10);
             }
             // third ,
             else if (comma_count == 2) {
-                parsed->patterns_size = (uint32_t)strtol(buffer, NULL, 10);
+                parsed->patterns_size = (uint32_t) strtol(buffer, NULL, 10);
             }
             else if (comma_count >= 3) {
                 // using comma_count as a counter from now on bcs why not
                 for (int j = 0; (j < cut_size) && (i + j < request_len); j++) {
                     // mat1
                     if (comma_count == 3) {
-                        parsed->mat1[matrix_index++] = (uint32_t) request[i + j];
+                        char* substring = request;
+                        strncpy(substring, buffer+(i+j), cut_size);
+                        parsed->mat1 = (uint32_t *)substring;
                     }
                     // mat2
                     else if (comma_count == 4) {
-                        parsed->mat2[matrix_index++] = (uint32_t) request[i + j];
+                        char* substring = request;
+                        strncpy(substring, buffer+(i+j), cut_size);
+                        parsed->mat2 = (uint32_t *)substring;
                     }
                     // patterns
                     else if (comma_count == 5) {
-                        parsed->patterns[pattern_index++] = (uint32_t) request[i + j];
+                        char* substring = request;
+                        strncpy(substring, buffer+(i+j), cut_size);
+                        parsed->patterns = (uint32_t *)substring;
                     }
                 }
                 i += cut_size - 1;
@@ -90,11 +96,20 @@ void parse_request(struct parsed_request *parsed, char *request, size_t request_
  * @note `result` should be modified to the result of the multiplication of the matrices
 */
 void multiply_matrix(uint32_t *matrix1, uint32_t *matrix2, uint32_t *result, uint32_t K) {
-    (void)matrix1;
-    (void)matrix2;
-    (void)result;
-    (void)K;
-    return;
+    // initialize KxK matrix
+    for(uint32_t i = 0; i < K; i++) {
+        for(uint32_t j = 0; j < K; j++) {
+            result[i*K + j] = 0;
+        }
+    }
+    // fill matrix
+    for(uint32_t i = 0; i < K; i++) {
+        for(uint32_t j = 0; j < K; j++) {
+            for (uint32_t k = 0; k < K; k++) {
+                result[i*K + j] += matrix1[i*K + k] * matrix2[k*K + j];
+            }
+        }
+    }
 }
 
 /**
@@ -110,8 +125,7 @@ void multiply_matrix(uint32_t *matrix1, uint32_t *matrix2, uint32_t *result, uin
  *
  * @note `file` should be modified to contain the encrypted file.
 */
-void test_patterns(uint32_t *matrix, uint32_t matrix_size, uint32_t *patterns,
-                   uint32_t pattern_size, uint32_t nb_patterns, uint32_t *res) {
+void test_patterns(uint32_t *matrix, uint32_t matrix_size, uint32_t *patterns, uint32_t pattern_size, uint32_t nb_patterns, uint32_t *res) {
     (void)matrix;
     (void)matrix_size;
     (void)patterns;
@@ -128,10 +142,30 @@ void test_patterns(uint32_t *matrix, uint32_t matrix_size, uint32_t *patterns,
  * @param res: The array to transform into a string
  * @param res_size: The length of the the array `res`
 */
-void res_to_string(char *str, uint32_t *res, uint32_t res_size) { 
-    (void)str;
-    (void)res;
-    (void)res_size;
-    printf("CHANGE ME\n"); 
+void res_to_string(char *str, uint32_t *res, uint32_t res_size) {
+    str[0] = '\0'; // otherwise it writes sh*t in the start
+    for (uint32_t i = 0; i < res_size; i++) {
+        char buffer[12];
+        // number => string_number
+        sprintf(buffer, "%u", res[i]);
+        // string_number => str
+        strcat(str, buffer);
+        // if not last => comma
+        if (i < res_size - 1) {
+            strcat(str, ",");
+        }
+    }
 }
 
+
+//int main() {
+//    uint32_t res_size = 3;
+//    uint32_t res[] = {15, 16, 17};
+//    char* string;
+//    for (int i = 0; i < res_size; i++) {
+//        printf("%d ", res[i]);
+//    }
+//    res_to_string(string, res, res_size);
+//    printf("\n%s", string);
+//    return 0;
+//}
