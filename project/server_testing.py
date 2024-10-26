9a1a8370e443dd8ed07fd28bc35e22dd42629ece
CC4K <cedric.kheirallah@student.uclouvain.be>
2024-10-26 23:09:41 +0200

    répétition des tests=>retourne une moyenne des résultats plutôt que 3 résultats

--- project/server_testing.py
+++ project/server_testing.py
@@ -1,5 +1,4 @@
 import subprocess
-from typing import List
 import pandas as pd
 
 factors = {
@@ -31,11 +30,7 @@ def launch_and_parse(**params):
         '--script', './wrk_scripts/simple_scenario.lua'
     ])
     return output
-    
-    
-    # f.write(str(output))
-    # f.close()
-    # should get the number of interest
+
 
 def design_experiment():
     X=[]
@@ -52,48 +47,6 @@ def design_experiment():
         X = newX
     return pd.DataFrame(X, columns = factors.keys())
 
-######################################################"
-
-n_repetitions = 3
-
-def launch_experiment(design) -> List[List]:
-    data = []
-    results = []
-    for run in design.to_dict(orient="records"):
-            print(f"Running {run}...")
-            for repet in range(n_repetitions):
-                r = launch_and_parse(**run)
-                print(r)
-                run_result = list(run.values()).copy()
-                run_result.append(r)
-                results.append(run_result)
-    return results
-
-# runs = design_experiment()
-# print(runs)
-# results = launch_experiment(runs)
-# results = pd.DataFrame(results, columns=list(factors.keys())+ ['Y'])
-# print(results)
-
-################
-
-#The first factor is used for the X axis
-x = list(factors.items())[0]
-#The second factor is used as series
-series = list(factors.items())[1]
-# print(x)
-# print(series)
-
-# g = sns.catplot(
-#     data=results, kind="bar",
-#     x=x[0], y="Y", hue=series[0],
-#     errorbar="sd"
-# )
-
-# for factor in factors:
-    # print(factors[factor])
-    # print(subprocess.run(["pwd"]))
-    
 repeat_count = 3
 df = design_experiment()
 
@@ -106,18 +59,20 @@ import re
 with open('results.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['MATSIZE', 'PATTERNS_SIZE', 'NB_PATTERNS', 'THREADS', 'CONNECTIONS', 'DURATION', 'RATE', 'Requests/sec', 'Transfer/sec'])
-    for j in range(repeat_count):    
-        for i in range(len(df)):
-            print("i: "+str(i)+" j: "+str(j))
-            df_params = df.iloc[i]
-            params = df_params.to_dict()
-            if(i<0):
-                continue
-            if(params["MATSIZE"]**2<params["PATTERNS_SIZE"]+1):
-                continue
-            if(params["threads"]>params["connections"]):
-                continue
-            try:
+    for i in range(len(df)):
+        df_params = df.iloc[i]
+        params = df_params.to_dict()
+        if(i<0):
+            continue
+        if(params["MATSIZE"]**2<params["PATTERNS_SIZE"]+1):
+            continue
+        if(params["threads"]>params["connections"]):
+            continue
+        try:
+            request = []
+            transfer = []
+            for j in range(repeat_count):
+                print("Test n°"+str(i)+"| Repeat n°"+str(j+1))
                 out = launch_and_parse(**params)
                 # only keep the line that starts with Requests/sec:
                 out = out.decode("utf-8").split("\n")
@@ -131,10 +86,14 @@ with open('results.csv', mode='w', newline='') as file:
                     if line.startswith("Transfer/sec:"):
                         match = line.split("Transfer/sec:")[1].replace(' ','')
                         transfer_per_second = match
-                # print("Request/sec : "+str(request_per_second)+" / Transfer/sec : "+str(transfer_per_second))
-                writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , request_per_second, transfer_per_second])
-                if(request_per_second<=0):
-                    print("test failed"+str(params))
-            except Exception as e:
-                writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , -1, -1])
+                request.append(request_per_second)
+                transfer.append(transfer_per_second)
+            rps = sum(request)/repeat_count
+            tps = sum(transfer)/repeat_count
+            # print("Request/sec : "+str(request_per_second)+" / Transfer/sec : "+str(transfer_per_second))
+            writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , rps, tps])
+            if(rps<=0):
+                print("test failed"+str(params))
+        except Exception as e:
+            writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , -1, -1])
 
