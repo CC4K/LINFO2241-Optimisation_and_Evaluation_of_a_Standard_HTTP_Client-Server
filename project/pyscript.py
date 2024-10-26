cd4398efd606faf1840095179e8bbdd846a86705
Anthony Guerrero Gurriaran <anthony.guerrero@student.uclouvain.be>
2024-10-26 19:27:08 +0000

    added re-runs

--- project/pyscript.py
+++ project/pyscript.py
@@ -133,7 +133,7 @@ series = list(factors.items())[1]
     # print(factors[factor])
     # print(subprocess.run(["pwd"]))
     
-    
+repeat_count = 3
 df = design_experiment()
 
 print("df made")
@@ -145,35 +145,35 @@ import re
 with open('results.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['MATSIZE', 'PATTERNS_SIZE', 'NB_PATTERNS', 'THREADS', 'CONNECTIONS', 'DURATION', 'RATE', 'Requests/sec', 'Transfer/sec'])
-
-    for i in range(len(df)):
-        print("i: "+str(i))
-        df_params = df.iloc[i]
-        params = df_params.to_dict()
-        if(i<0):
-            continue
-        if(params["MATSIZE"]**2<params["PATTERNS_SIZE"]+1):
-            continue
-        if(params["threads"]>params["connections"]):
-            continue
-        try:
-            out = launch_and_parse(**params)
-            # only keep the line that starts with Requests/sec:
-            out = out.decode("utf-8").split("\n")
-            # print(out)
-            request_per_second = -1
-            transfer_per_second = -1
-            for line in out:
-                if line.startswith("Requests/sec:"):
-                    match = re.search(r'[-+]?\d*\.\d+|\d+', line)
-                    request_per_second = float(match.group())
-                if line.startswith("Transfer/sec:"):
-                    match = line.split("Transfer/sec:")[1].replace(' ','')
-                    transfer_per_second = match
-            # print("Request/sec : "+str(request_per_second)+" / Transfer/sec : "+str(transfer_per_second))
-            writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , request_per_second, transfer_per_second])
-            if(request_per_second<=0):
-                print("test failed"+str(params))
-        except Exception as e:
-            writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , -1, -1])
+    for j in range(repeat_count):    
+        for i in range(len(df)):
+            print("i: "+str(i)+" j: "+str(j))
+            df_params = df.iloc[i]
+            params = df_params.to_dict()
+            if(i<0):
+                continue
+            if(params["MATSIZE"]**2<params["PATTERNS_SIZE"]+1):
+                continue
+            if(params["threads"]>params["connections"]):
+                continue
+            try:
+                out = launch_and_parse(**params)
+                # only keep the line that starts with Requests/sec:
+                out = out.decode("utf-8").split("\n")
+                # print(out)
+                request_per_second = -1
+                transfer_per_second = -1
+                for line in out:
+                    if line.startswith("Requests/sec:"):
+                        match = re.search(r'[-+]?\d*\.\d+|\d+', line)
+                        request_per_second = float(match.group())
+                    if line.startswith("Transfer/sec:"):
+                        match = line.split("Transfer/sec:")[1].replace(' ','')
+                        transfer_per_second = match
+                # print("Request/sec : "+str(request_per_second)+" / Transfer/sec : "+str(transfer_per_second))
+                writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , request_per_second, transfer_per_second])
+                if(request_per_second<=0):
+                    print("test failed"+str(params))
+            except Exception as e:
+                writer.writerow([params["MATSIZE"], params["PATTERNS_SIZE"], params["NB_PATTERNS"],params["threads"],params["connections"],params["duration"],params["rate"] , -1, -1])
 
