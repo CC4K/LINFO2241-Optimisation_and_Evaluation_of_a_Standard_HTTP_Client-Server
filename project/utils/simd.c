92c3990fd72ca6ad0f17d7f32767e976253ae644
CC4K <cedric.kheirallah@student.uclouvain.be>
2024-12-09 22:47:19 +0100

    functional AVX256

--- project/utils/simd.c
+++ project/utils/simd.c
@@ -7,26 +7,40 @@
 // make -B run_release_simd CFLAGS+="-DSIMDBEST"
 
 void multiply_matrix_simd(uint32_t *matrix1, uint32_t *matrix2, uint32_t *result, uint32_t K) {
-    (void)matrix1;
-    (void)matrix2;
-    (void)result;
-    (void)K;
-
 #if defined SIMD128
     // Use SSE
     #error SIMD128 Not implemented
 #elif defined SIMD256
     // Use AVX2
-    // TODO: 256 first then super EZ PZ
     for (uint32_t i = 0; i < K; i++) {
         for (uint32_t j = 0; j < K; j++) {
-            for (uint32_t k = 0; k < K; k++) {
-                result[i * K + j] += matrix1[i * K + k] * matrix2[k * K + j];
+            __m256i sum_vec = _mm256_setzero_si256();
+            uint32_t k = 0;
+            for (; k + 7 < K; k += 8) {
+                // load 8 elements from matrix1 row and matrix2 column
+                __m256i vec1 = _mm256_loadu_si256((__m256i*)&matrix1[i * K + k]);
+                __m256i vec2 = _mm256_set_epi32(
+                    matrix2[j + (k + 7) * K], matrix2[j + (k + 6) * K],
+                    matrix2[j + (k + 5) * K], matrix2[j + (k + 4) * K],
+                    matrix2[j + (k + 3) * K], matrix2[j + (k + 2) * K],
+                    matrix2[j + (k + 1) * K], matrix2[j + k * K]);
+                // multiply and accumulate
+                __m256i prod = _mm256_mullo_epi32(vec1, vec2);
+                sum_vec = _mm256_add_epi32(sum_vec, prod);
             }
+            uint32_t sum = 0;
+            uint32_t temp[8];
+            _mm256_storeu_si256((__m256i*)temp, sum_vec);
+            for (int x = 0; x < 8; x++) {
+                sum += temp[x];
+            }
+            // rest
+            for (; k < K; k++) {
+                sum += matrix1[i * K + k] * matrix2[j + k * K];
+            }
+            result[i * K + j] = sum;
         }
     }
-
-
 #elif defined SIMD512 || defined SIMDBEST
     // Use AVX512
     #error SIMD512 Not implemented
